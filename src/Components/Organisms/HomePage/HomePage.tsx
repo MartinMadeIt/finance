db7ef8dde6237ce6726eb6aa085606c22967f2a5
MartinMadeIt <marcinvet94@gmail.com>
2023-01-26 17:07:10 +0100

    Home Page styles rebuild

--- src/Components/Organisms/HomePage/HomePage.tsx
+++ src/Components/Organisms/HomePage/HomePage.tsx
@@ -10,42 +10,38 @@ function HomePage() {
 
 
   return (
-    <div className={styles.container}>
+    <div className={styles.home}>
       <NavbarLogged />
       
-      <div className={styles.bg}>
-        <div className={styles.loggedContent}>
+      <div className={styles.background}>
+        <div className={styles.content}>
           <p className={styles.marker}>.</p>
           <motion.div
-            className={styles.expLink}
+            className={styles.content__expenditures}
             variants={tileUp}
             initial="initial"
             animate="inView"
             >
-            <Link to={'/expenditures'} className={styles.expCard}>
-              <div className={styles.expImg} />
-              <div className={styles.textCard}>
-                <p className={styles.cardHeader}>Expenditures</p>
-                <p className={styles.cardDesc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, 
-                rem. Magnam, nisi? Provident eveniet saepe itaque at. Sed et consequuntur, tenetur, illo nam sit 
-                quos ab magni facilis, necessitatibus earum.</p>
+            <Link to={'/expenditures'} className={styles.card}>
+              <div className={styles.card__image} />
+              <div className={styles.card__text}>
+                <h3 className={styles.card__header}>Expenditures</h3>
+                <p className={styles.card__description}>Track your expenditures. Remember to fill data every day for better statistics of your expenditures.</p>
               </div>
             </Link>
           </motion.div>
 
           <motion.div
-            className={styles.loanLink}
+            className={styles.content__loan}
             variants={tileDown}
             initial="initial"
             animate="inView"
           >
-            <Link to={'/loan'} className={styles.loanCard}>
-              <div className={styles.loanImg} />
-              <div className={styles.textCard}>
-                <p className={styles.cardHeader}>Loan Tracker</p>
-                <p className={styles.cardDesc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, 
-                rem. Magnam, nisi? Provident eveniet saepe itaque at. Sed et consequuntur, tenetur, illo nam sit 
-                quos ab magni facilis, necessitatibus earum.</p>
+            <Link to={'/loan'} className={styles.card}>
+              <div className={styles.card__image} />
+              <div className={styles.card__text}>
+                <p className={styles.card__header}>Loan Tracker</p>
+                <p className={styles.card__description}>Do you have some loans ? Add them to track, give them a payment info and track your payment history and how much to pay left !</p>
               </div>
             </Link>
           </motion.div>
